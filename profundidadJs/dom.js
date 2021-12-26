20b899520ffb7240a6f3c1bb6605bfa1cb7cf3f2
angelDariaux <angeldariaux@gmail.com>
2021-12-26 10:39:16 -0600

    usado el querySelector y cambiados los colores usando JS

--- profundidadJs/dom.js
+++ profundidadJs/dom.js
@@ -1,2 +1,27 @@
 'use strict'
 
+// DOM - Document Object Model 
+
+const cambiaColor = (color) => {
+  caja.style.background = color;
+}
+
+
+let caja = document.querySelector('#miCaja');
+let caja1 = document.querySelector('#miCaja').innerHTML; //Devuelve el texto que esta dentro del HTML
+caja.innerHTML = 'TEXTO EN LA CAJA DE JS!!!';
+
+        //Cambiamos los estilos del HTML 
+
+caja.style.background = 'red';
+caja.style.padding = '20px';
+caja.style.color = 'white';
+cambiaColor('green');
+
+        //Modificar las clases y ID
+
+caja.className = 'hola hola2';
+
+
+console.log(caja);
+console.log(caja1);
