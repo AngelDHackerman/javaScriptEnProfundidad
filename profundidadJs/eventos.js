8e8c92166849e864e82539925c7db651e4baf30f
angelDariaux <angeldariaux@gmail.com>
2021-12-27 18:28:18 -0600

    Agregado el Evento Load

--- profundidadJs/eventos.js
+++ profundidadJs/eventos.js
@@ -2,69 +2,77 @@
 
 //Eventos, es una funcion que se ejecuta cuando sucede algo.
 
-      //Eventos de raton.
+      //Evento Load, verifica que el HTML este listo y cargado antes de empezar todo el codigo
 
-const cambiarColor = () => { 
-  console.log('Me has dado click');
+window.addEventListener('load', () => {
 
-  let bg = boton.style.background;
-  if (bg == 'green') {
-    boton.style.background = 'red';
-  } else if (bg == 'green' || bg == 'red') { 
-    boton.style.background = 'yellow';
-  } else  { 
-    boton.style.background = 'green';
+        //Eventos de raton.
+
+  const cambiarColor = () => { 
+    console.log('Me has dado click');
+
+    let bg = boton.style.background;
+    if (bg == 'green') {
+      boton.style.background = 'red';
+    } else if (bg == 'green' || bg == 'red') { 
+      boton.style.background = 'yellow';
+    } else  { 
+      boton.style.background = 'green';
+    };
+
+    boton.style.padding = '10px';
+    boton.style.border = '1px solid #ccc';
   };
 
-  boton.style.padding = '10px';
-  boton.style.border = '1px solid #ccc';
-};
+  let boton = document.querySelector('#boton'); 
+  let input = document.querySelector('#campoNombre');
 
-let boton = document.querySelector('#boton'); 
-let input = document.querySelector('#campoNombre');
+            //Evento Click
+  boton.addEventListener('click', (() => { //se usa una funcion de callBack para ejecutar la funcion que necesitamos
+    cambiarColor();
+  }));
 
-          //Evento Click
-boton.addEventListener('click', (() => { //se usa una funcion de callBack para ejecutar la funcion que necesitamos
-  cambiarColor();
-}));
+            //Evento Mouse over 
+  boton.addEventListener('mouseover', () => { 
+    boton.style.background = '#2ECC71'
+  });
 
-          //Evento Mouse over 
-boton.addEventListener('mouseover', () => { 
-  boton.style.background = '#2ECC71'
-});
+            //Evento Mouse out
 
-          //Evento Mouse out
+  boton.addEventListener('mouseout', () => { 
+    boton.style.background = '#30475E';
+  });
 
-boton.addEventListener('mouseout', () => { 
-  boton.style.background = '#30475E';
-});
+        //Eventos de teclado
 
-          //Focus
+            //Focus
 
-input.addEventListener('focus', () => { 
-  console.log('Event focus, Estas dentro del input');
-});
+  input.addEventListener('focus', () => { 
+    console.log('Event focus, Estas dentro del input');
+  });
 
-          //Blur, es lo mismo que Focus pero este se activa cuando salimos del foco
+            //Blur, es lo mismo que Focus pero este se activa cuando salimos del foco
 
-input.addEventListener('blur', () => { 
-  console.log('Event Blur, Estas fuera del input');
-});
+  input.addEventListener('blur', () => { 
+    console.log('Event Blur, Estas fuera del input');
+  });
 
-          //Keydown
+            //Keydown
 
-input.addEventListener('keydown', (event) => { 
-  console.log('Event Keydown, Estas pulsando esta tecla:', String.fromCharCode(event.keyCode));// asi sacamos que teclas se estan precionando
-});
+  input.addEventListener('keydown', (event) => { 
+    console.log('Event Keydown, Estas pulsando esta tecla:', String.fromCharCode(event.keyCode));// asi sacamos que teclas se estan precionando
+  });
 
-          //Keypress
+            //Keypress
 
-input.addEventListener('keypress', (event) => { 
-  console.log('Event Keypress, Estas pulsando esta tecla:', String.fromCharCode(event.keyCode));// asi sacamos que teclas se estan precionando
-});
+  input.addEventListener('keypress', (event) => { 
+    console.log('Event Keypress, Estas pulsando esta tecla:', String.fromCharCode(event.keyCode));// asi sacamos que teclas se estan precionando
+  });
 
-          //Keyup
+            //Keyup
 
-input.addEventListener('keyup', (event) => { 
-  console.log('Event Keyup, Tecla soltada:', String.fromCharCode(event.keyCode));// asi sacamos que teclas se estan precionando
-});
+  input.addEventListener('keyup', (event) => { 
+    console.log('Event Keyup, Tecla soltada:', String.fromCharCode(event.keyCode));// asi sacamos que teclas se estan precionando
+  });
+
+}); //End of load
