35a9eafeeeaadffccf7851ca1a4789980b333bc2
angelDariaux <angeldariaux@gmail.com>
2022-01-02 17:49:38 -0600

    terminada la clase del localStorage

--- localStorage/localJS/localStorage.js
+++ localStorage/localJS/localStorage.js
@@ -11,6 +11,7 @@ if(typeof(Storage) !== 'undefined') {
 };
 
           //Guardar datos en el localStorage
+
           //localStorage.setItem(titulo, valorDelItem)
 
 localStorage.setItem('titulo', 'curso de Synfony de victor Robles');
@@ -21,6 +22,40 @@ console.log(localStorage.getItem('titulo'));
 
           //Agregar LocalStorage a la pagina
 document.querySelector('#parrafo').innerHTML = localStorage.getItem('titulo'); 
+
+          //GUARDAR OBJETOS EN EL LOCALSTORAGE
+
+let usuario = { 
+  nombre: 'Angel Hernandez',
+  email: 'angel@angel.com',
+  web: 'angelWeb.com',
+};
+
+localStorage.setItem('usuario', JSON.stringify(usuario));//usando JSON.stringify, convertimos el objeto JSon a un string para poder almancenarlo
+
+
+        //  RECUPERA EL OBJETO DE LOCALSTORAGE
+
+console.group(`Recuperando el objeto del localStora`)
+
+  console.log(typeof(localStorage.getItem('usuario')), localStorage.getItem('usuario'));
+
+let usuarioParse = JSON.parse(localStorage.getItem('usuario'));//con JSON.parse pasamos de un String a un Objeto JSON
+  console.log(typeof(usuarioParse), usuarioParse);
+
+document.querySelector('#datos').append(usuarioParse.web);
+console.groupEnd();
+
+        //Remover el localStorage
+
+localStorage.removeItem('usuario'); //Lo eliminamos del localStorage
+localStorage.clear(); //Eliminamos todo el localStorage
+
+
+
+
+
+
 
 
 
