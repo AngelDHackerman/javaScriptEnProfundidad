3215801ad40e7effff326181cde2342571c6aac1
angelDariaux <angeldariaux@gmail.com>
2022-01-04 18:01:36 -0600

    terminada la seccion de promesas y fetch

---
+++ fetchYPromesas/datosExternosJS/tryCatch.js
@@ -0,0 +1,47 @@
+'use strict'
+window.addEventListener('load', () => { 
+
+try {
+  let year = 2022;
+  alert(year);
+  console.log(decodeURIComponent('https://goo$$$$gle.com'));
+
+} catch (error) {
+  console.log(error)
+  alert(`ha ocurrido un error`);
+}
+
+
+console.log('se siguio ejecutando el codigo, sin el try catch esto no seria posible');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
