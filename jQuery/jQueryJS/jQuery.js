98a95f3eac7e6c894e50fa81006355d440e9006d
angelDariaux <angeldariaux@gmail.com>
2022-01-04 22:53:11 -0600

    descargado jQuery en nuestra pagina web

---
+++ jQuery/jQueryJS/jQuery.js
@@ -0,0 +1,52 @@
+'use strict'
+window.addEventListener('load', () => { 
+
+$(document).ready(function() { 
+  console.log('jQuery y la web cargados')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
